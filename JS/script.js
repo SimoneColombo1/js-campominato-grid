a1dafd4bf4199e69a4920a632b0921a52f6ad81b
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-20 02:01:57 +0200

    creato il ciclo for e e la variabile per le caselle

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,29 @@ const griglia= document.querySelector('section#griglia');
 const StartButton= document.querySelector('section#start');
 
 //!evento// 
+
+
+StartButton.addEventListener('click',function(){
+
+
+
+    //? creazione quadrati//
+const quadrati= document.createElement('div');
+quadrati.classList.add('square');
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+)
