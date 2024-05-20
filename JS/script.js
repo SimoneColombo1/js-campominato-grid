9d3404ae53ea981229289d7d4561cb095ca495a9
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-20 02:11:25 +0200

    creati gli span contenenti i numeri

--- JS/script.js
+++ JS/script.js
@@ -8,11 +8,35 @@ const StartButton= document.querySelector('section#start');
 
 StartButton.addEventListener('click',function(){
 
+//todo ciclo for//
+
+
+for( let i=0 ; i<100;i++ ){
+
 
 
     //? creazione quadrati//
 const quadrati= document.createElement('div');
 quadrati.classList.add('square');
+
+//$ creazione span con i numeri//
+ 
+const elementi= document.createElement('span');
+elementi.append(i+1);
+quadrati.appendChild('elementi');
+
+
+//^ Colorare quadrati al clicl//
+
+quadrati.addEventListener('click')
+
+
+}
+
+
+
+
+
 
 
 
